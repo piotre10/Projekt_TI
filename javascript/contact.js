a02de8daee63ac6f4d347dab29484197400c89bd
rad-mat <mradek2909@gmail.com>
2021-12-25 20:27:06 +0100

    contact 1.0

---
+++ javascript/contact.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+  /* przepraszam, że jest to takie głupie :( */
+  
+  $('#layout_contact_data table tr:nth-child(1)').mouseenter(function(){ 
+    $('#layout_contact_data table tr:nth-child(1) td:first').css({ 'font-size': '30px', 'transition' : '0.2s' }); 
+  }); 
+  
+  $('#layout_contact_data table tr:nth-child(1)').mouseleave(function(){ 
+    $('#layout_contact_data table tr:nth-child(1) td:first').css({ 'font-size': '20px', 'transition' : '0.2s' }); 
+  }); 
+
+  $('#layout_contact_data table tr:nth-child(2)').mouseenter(function(){ 
+    $('#layout_contact_data table tr:nth-child(2) td:first').css({ 'font-size': '30px', 'transition' : '0.2s' }); 
+  }); 
+  
+  $('#layout_contact_data table tr:nth-child(2)').mouseleave(function(){ 
+    $('#layout_contact_data table tr:nth-child(2) td:first').css({ 'font-size': '20px', 'transition' : '0.2s' }); 
+  }); 
+
+  $('#layout_contact_data table tr:nth-child(3)').mouseenter(function(){ 
+    $('#layout_contact_data table tr:nth-child(3) td:first').css({ 'font-size': '30px', 'transition' : '0.2s' }); 
+  }); 
+  
+  $('#layout_contact_data table tr:nth-child(3)').mouseleave(function(){ 
+    $('#layout_contact_data table tr:nth-child(3) td:first').css({ 'font-size': '20px', 'transition' : '0.2s' }); 
+  }); 
+
+  $('#layout_contact_data table tr:nth-child(4)').mouseenter(function(){ 
+    $('#layout_contact_data table tr:nth-child(4) td:first').css({ 'font-size': '30px', 'transition' : '0.2s' }); 
+  }); 
+  
+  $('#layout_contact_data table tr:nth-child(4)').mouseleave(function(){ 
+    $('#layout_contact_data table tr:nth-child(4) td:first').css({ 'font-size': '20px', 'transition' : '0.2s' }); 
+  }); 
+});
