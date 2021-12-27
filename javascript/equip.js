4891cab856db3cca79a1fde7f029ff0c284573da
rad-mat <mradek2909@gmail.com>
2021-12-27 23:47:00 +0100

    equip lvl up

--- javascript/equip.js
+++ javascript/equip.js
@@ -1,106 +1,94 @@
-var suma = 0;
-var plecak = false;
-var termos = false;
-var śpiwór = false;
-var namiot = false;
-var raki = false;
-var czekan = false;
+let suma = 0;
+let items = [];
 
-$(document).ready(function() {
-    function updateSum(number) {
-        suma += number;
-        if (suma > 250) {
-            $('#discount').text(suma + " zł");
-            $('#normal').text(suma*0.9 + " zł");
-        } else {
-            $('#discount').text("");
-            $('#normal').text(suma + " zł");
-        }
-    }
-    function insertItem(item_name, item_price) {
-        var item_quantity = prompt("Podaj ilość:");
-        if(item_quantity < 1 | item_quantity > 6) return 0;
-        
-        $('#icon_' + item_name).attr('class', 'fas fa-times');
-        $('#txt_' + item_name).text('Usuń z koszyka ');
+function pushItem(item, price){
+    if(items.includes(item)) increment(item, price);
+    else add_to_cart(item, price);
+}
 
-        var newItem = $('<li></li>');
-        $(newItem).text(item_quantity + ' x ' + item_name);
-        $(newItem).attr('id', 'list_'+ item_name);
-        newItem.insertAfter('#items ul li:last');
+function popItem(item){
+    decrement(item);
+}
 
-        var newPrice = $('<li></li>');
-        $(newPrice).text(item_price*item_quantity + ' zł');
-        $(newPrice).attr('id', 'list_' + item_name + '_price');
-        newPrice.insertAfter('#prices ul li:last');
+function add_to_cart(item_name, item_price) {
+    document.getElementById(item_name).lastElementChild.classList.add('active');
 
-        updateSum(item_price*item_quantity);
-        return 1;
-    }
-    function deleteItem(item_name) {
-        $('#icon_' + item_name).attr('class', 'fas fa-shopping-cart');
-        $('#txt_' + item_name).text('Dodaj do koszyka ');
+    let newItem = $('<li></li>');
+    $(newItem).text(1 + ' x ' + item_name);
+    $(newItem).attr('id', 'list_'+ item_name);
+    newItem.insertAfter('#items ul li:last');
 
-        var price = $('#list_' + item_name + '_price').text().split(" ");
-        updateSum(-parseInt(price[0]));
+    let newPrice = $('<li></li>');
+    $(newPrice).text(item_price + ' zł');
+    $(newPrice).attr('id', 'list_' + item_name + '_price');
+    newPrice.insertAfter('#prices ul li:last');
 
-        $('#list_' + item_name).remove();
-        $('#list_' + item_name + '_price').remove();
-    }
+    updateSum(item_price);
+    items.push(item_name);
+}
 
-    updateSum(0);
-    $('#cart_plecak').click(function() {
-        if(!plecak) {
-            if( insertItem("plecak", 30) ) plecak = true;
-        } else {
-            deleteItem("plecak");
-            plecak = false;
-        }     
-    }); //koniec plecak.click()
-
-    $('#cart_termos').click(function() {
-        if(!termos) {
-            if( insertItem("termos", 10) ) termos = true;
-        } else {
-            deleteItem("termos");
-            termos = false;
-        }    
-    }); //koniec termos.click()
+function increment(item_name, item_price){
+    let item = document.getElementById('list_' + item_name);
+    let price = document.getElementById('list_' + item_name + '_price');
+    let quantity = parseInt(item.textContent.split()[0]);
     
-    $('#cart_śpiwór').click(function() {
-        if(!śpiwór) {
-            if( insertItem("śpiwór", 35) ) śpiwór = true;
-        } else {
-            deleteItem("śpiwór");
-            śpiwór = false;
-        }    
-    }); //koniec śpiwór.click()
+    if(quantity == 6) return;
+    quantity++;
+    if(quantity == 6) document.getElementById(item_name).firstElementChild.classList.remove('active');
 
-    $('#cart_namiot').click(function() {
-        if(!namiot) {
-            if( insertItem("namiot", 80) ) namiot = true;
-        } else {
-            deleteItem("namiot");
-            namiot = false;
-        }    
-    }); //koniec namiot.click()
+    item.textContent = (quantity + ' x ' + item_name);
+    price.textContent = (quantity*item_price + ' zł');
+    updateSum(item_price);
+}
 
-    $('#cart_raki').click(function() {
-        if(!raki) {
-            if( insertItem("raki", 40) ) raki = true;
-        } else {
-            deleteItem("raki");
-            raki = false;
-        }    
-    }); //koniec raki.click()
+function decrement(item_name){
+    let item = document.getElementById('list_' + item_name);
+    let price = document.getElementById('list_' + item_name + '_price');
+    let quantity = parseInt(item.textContent.split()[0]);
+    let item_price = parseFloat(price.textContent.split()[0]) / quantity;
 
-    $('#cart_czekan').click(function() {
-        if(!czekan) {
-            if( insertItem("czekan", 35) ) czekan = true;
-        } else {
-            deleteItem("czekan");
-            czekan = false;
-        }    
-    }); //koniec czekan.click()
-}); //koniec ready
+    if(quantity == 0) return;
+    quantity--;
+    if(quantity == 0){
+        document.getElementById(item_name).lastElementChild.classList.remove('active');
+        delete_from_cart(item_name);
+        items.splice(items.indexOf(item), 1);
+        return;
+    }
+    if(quantity == 5) document.getElementById(item_name).firstElementChild.classList.add('active');
 
+    item.textContent = (quantity + ' x ' + item_name);
+    price.textContent = (quantity*item_price + ' zł');
+    updateSum(-item_price);
+}
+
+function delete_from_cart(item_name) {
+    $('#icon_' + item_name).attr('class', 'fas fa-shopping-cart');
+    $('#txt_' + item_name).text('Dodaj do koszyka ');
+
+    var price = $('#list_' + item_name + '_price').text().split(" ");
+    updateSum(-parseInt(price[0]));
+
+    $('#list_' + item_name).remove();
+    $('#list_' + item_name + '_price').remove();
+}
+
+function updateSum(number) {
+    suma += number;
+    let discount_price = document.getElementById('discount');
+    let normal_price = document.getElementById('normal');
+    if (suma > 250) {
+        discount_price.textContent = (suma + " zł");
+        normal_price.textContent = (suma*0.9 + " zł");
+    } else {
+        discount_price.textContent = '';
+        normal_price.textContent = (suma + " zł");
+    }
+}
+
+/* You can file your order only if any item is selected and any txt entered */
+function orderCheck(){
+    if(document.getElementById('email_box').value && suma){
+        window.open('order.html', target='_self');
+    }
+}
